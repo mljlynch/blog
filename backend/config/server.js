268410991f9724155397191ca5a43528ecfba281
aharabiosciences <jacoblync@gmail.com>
2020-11-21 17:33:57 -0500

    initial commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd9bcb6972970a97a04033d79584c3046'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3dfc4281-cc92-432d-b612-f0c6834a6f51'
+};
